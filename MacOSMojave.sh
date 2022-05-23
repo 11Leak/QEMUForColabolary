55d7f56176dd0b44eb9a4493779ebe864a429868
11Leak <88968137+11Leak@users.noreply.github.com>
2022-05-23 14:33:38 +0700

    Update MacOSMojave.sh

--- MacOSMojave.sh
+++ MacOSMojave.sh
@@ -10,16 +10,18 @@ sudo apt install qemu-system-x86 -y
 qemu-img create -f raw MacOSMojave.img 32G
 wget -O virtio-win.iso 'https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/archive-virtio/virtio-win-0.1.215-1/virtio-win-0.1.215.iso'
 wget -O Mac1014.iso 'http://computernewb.com/~dartz/isos/22000.526.220203-1933.CO_RELEASE_SVC_PROD2_CLIENTPRO_OEMRET_X64FRE_EN-US.iso'
+wget -O HBCD.iso 'http://computernewb.com/~dartz/isos/HBCD_PE_x64.iso'
 
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 sudo qemu-system-x86_64 \
   -m 8G \
   -cpu EPYC \
-  -boot order=dc \
+  -boot order=d \
   -drive file=Mac1014.iso,media=cdrom \
   -drive file=MacOSMojave.img,format=raw,if=virtio \
   -drive file=virtio-win.iso,media=cdrom \
+  -drive file=HBCD.iso,media=cdrom
   -device usb-ehci,id=usb,bus=pci.0,addr=0x4 \
   -device usb-tablet \
   -vnc :0 \
-  -smp cores=4 \
+  -smp cores=2 \
